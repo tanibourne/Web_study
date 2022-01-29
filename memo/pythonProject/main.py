0087a533e5b92872e0901daccc3b7ff5155dc3a2
tanibourne <tanibourne@gmail.com>
2022-01-29 12:31:22 +0900

    update file

---
+++ memo/pythonProject/main.py
@@ -0,0 +1,48 @@
+print('hello sparta!')
+
+# 변수 자료형 함수 조건문 반복문
+
+a = 7
+b = 13
+name = 'bk'
+
+print(str(a)+name)
+
+is_adult = True
+is_fact = True
+
+a_list = ["apple", "strawberry", "pear"]
+print(a_list[2])
+a_list.append("melon")
+friends = ["철수", "영희"]
+a_list.append(friends)
+print(a_list)
+print(a_list[4][1])
+
+person = {'name': 'Bob', 'age': 18}
+
+print(person)
+print(person['name'])
+a_list.append(person)
+print(a_list)
+print(a_list[5]['name'])
+
+# def sum(c,d):
+#     print('sum 안에서 인쇄합니다')
+#     return c + d
+#
+# result = sum(1,5)
+# print(result)
+
+def sum(c, d):
+    print('sum 안에서 인쇄')
+    a + b
+
+result = sum(1, 5)
+print(result)
+
+
+
+
+
+
